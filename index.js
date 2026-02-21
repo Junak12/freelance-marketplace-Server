b672217989f36d6eda712a4148b915c83c799706
Junak12 <pauljunak@gmail.com>
2026-02-21 23:05:07 +0600

    creating a usercollection and post api for collecting users

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
       res.send(topReviews);
     })
 
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+
+    })
+
 
 
 
