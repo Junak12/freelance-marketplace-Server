2c4dc36d9b5bd928cda40c8902788fff346ddc73
Junak12 <pauljunak@gmail.com>
2026-03-07 01:48:58 +0600

    ok

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -5,6 +5,9 @@ dotenv.config();
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+// 🔴 FIX PRIVATE KEY FORMAT
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
